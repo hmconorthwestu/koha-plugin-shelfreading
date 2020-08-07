7711a5cde4971736e39f898ef7f02dbadfe350fb
hmconorthwestu <hannah.co@northwestu.edu>
2020-08-07 11:09:12 -0700

    add next_item sort inside unless $i == 0 . If this errors, may need to test that next item exists before checking sort

--- Koha/Plugin/Com/NULibrary/ShelfReading.pm
+++ Koha/Plugin/Com/NULibrary/ShelfReading.pm
@@ -254,13 +254,13 @@ sub inventory2 {
         				additemtobarcodes($item,@barcodes);
             }
         }
-        unless ( $i == scalar(@sortbarcodes) ) {
-            my $next_item = $sortbarcodes[ $i + 1 ];
-            if ( $next_item && $item->{cn_sort} gt $next_item->{cn_sort} ) {
-                $item->{problems}->{out_of_order} = 1;
-				additemtobarcodes($item,@barcodes);
-            }
-        }
+
+          my $next_item = $sortbarcodes[ $i + 1 ];
+          if ( $next_item && $item->{cn_sort} gt $next_item->{cn_sort} ) {
+              $item->{out_of_order} = 1;
+              additemtobarcodes($item,@barcodes);
+          }
+
 	}
 
 	#end of checks
